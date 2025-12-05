f5a3b6e2df0906b02ab48a9468699a5792cef227
Nate Nguyen <nathannguyen555@gmail.com>
2025-12-04 19:38:23 -0600

    Expanding category controller and added update at property to AppDBContext model

---
+++ Migrations/20251205013226_updated-at-property.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedatproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "category",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555555"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "category",
+                keyColumn: "id",
+                keyValue: new Guid("66666666-6666-6666-6666-666666666666"),
+                column: "updated_at",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "category");
+        }
+    }
+}
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -21,12 +21,12 @@ namespace ExpenseTrackerAPI.Models
             modelBuilder.Entity<Category>().ToTable("category");
 
             modelBuilder.Entity<Category>().HasData(
-                new { id = Guid.Parse("11111111-1111-1111-1111-111111111111"), name = "Housing", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
-                new { id = Guid.Parse("22222222-2222-2222-2222-222222222222"), name = "Rent", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
-                new { id = Guid.Parse("33333333-3333-3333-3333-333333333333"), name = "Savings", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
-                new { id = Guid.Parse("44444444-4444-4444-4444-444444444444"), name = "Transportation", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
-                new { id = Guid.Parse("55555555-5555-5555-5555-555555555555"), name = "Shopping", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
-                new { id = Guid.Parse("66666666-6666-6666-6666-666666666666"), name = "Utilities", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() }
+                new { id = Guid.Parse("11111111-1111-1111-1111-111111111111"), name = "Housing", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
+                new { id = Guid.Parse("22222222-2222-2222-2222-222222222222"), name = "Rent", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
+                new { id = Guid.Parse("33333333-3333-3333-3333-333333333333"), name = "Savings", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
+                new { id = Guid.Parse("44444444-4444-4444-4444-444444444444"), name = "Transportation", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
+                new { id = Guid.Parse("55555555-5555-5555-5555-555555555555"), name = "Shopping", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() },
+                new { id = Guid.Parse("66666666-6666-6666-6666-666666666666"), name = "Utilities", created_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime(), updated_at = DateTime.Parse("2024-01-01T00:00:00Z").ToUniversalTime() }
             );
         }
     }
